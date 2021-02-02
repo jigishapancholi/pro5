24dfa432890d7c873fced57d17df71f39af21f4d
jigishapancholi <78212995+jigishapancholi@users.noreply.github.com>
2021-02-02 22:21:31 +0530

    Create GoodDay.java

---
+++ GoodDay.java
@@ -0,0 +1,8 @@
+package com.sanumala;
+public class GoodDay
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello!! Have a Good Day...");   
+      }
+} 
